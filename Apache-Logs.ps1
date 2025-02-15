d09b020c6b8dd7a2f569299f6d28ab7769ec9bbd
csschn <connor.schnackenberg@mymail.champlain.edu>
2025-02-15 00:38:49 -0500

    apache logs assignment

---
+++ Apache-Logs.ps1
@@ -0,0 +1,24 @@
+﻿clear
+
+function apacheLogsFunction ($Page, $HTTPCode, $Browser){
+    $filePath = "C:\xampp\apache\logs\access.log"
+    $apacheLogs = Get-Content $filePath
+    $apacheLogs[1]
+
+    $finalIps = foreach ($line in $apacheLogs){
+            $portions = $line -split ' '
+
+            $ip = $portions[0]
+
+            $apachePage = $portions[6].Trim(' ')
+
+            $apacheCode = $portions[8]
+
+            $apacheBrowser = ($portions[11..($portions.Length-2)] -join ' ').Substring(1,($portions[11..($portions.Length-2)] -join ' ').Length-2)
+            $test
+            if ($apachePage -eq $Page -and $apacheCode -eq $HTTPCode -and $apacheBrowser -ilike "*$Browser*") {
+                $ip
+            }
+    }
+    return $finalIps
+}
---
+++ main.ps1
@@ -0,0 +1,8 @@
+﻿clear
+
+. C:\Users\champuser\SYS320-01\Apache-Logs.ps1
+
+$apacheAccess = apacheLogsFunction("/index.html",404,"Chrome")
+$ipCount = $apacheAccess | Where-Object { $_.IP -ilike "10.*" }
+$counts = $ipCount | Group-Object -Property IP
+$counts | Select-Object Count, Name
