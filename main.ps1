70cdfc5504ea4b6586b8ee50f5dbe7cb08952154
csschn <connor.schnackenberg@mymail.champlain.edu>
2025-02-15 00:47:09 -0500

    main push

--- main.ps1
+++ main.ps1
@@ -1,8 +1,12 @@
 ﻿clear
 
 . C:\Users\champuser\SYS320-01\Apache-Logs.ps1
+. C:\Users\champuser\SYS320-01\apachelogsparse.ps1
 
 $apacheAccess = apacheLogsFunction("/index.html",404,"Chrome")
 $ipCount = $apacheAccess | Where-Object { $_.IP -ilike "10.*" }
 $counts = $ipCount | Group-Object -Property IP
 $counts | Select-Object Count, Name
+
+$result = ApacheLogs1
+$result
