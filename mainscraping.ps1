b1cf4621ec22e3fda7a6884a237c15434350187e
csschn <connor.schnackenberg@mymail.champlain.edu>
2025-03-05 20:19:10 -0500

    scraping classes commit

--- mainscraping.ps1
+++ mainscraping.ps1
@@ -1,6 +1,32 @@
-﻿. C:\Users\champuser\SYS320-01\scrapingclasses.ps1
+﻿. (Join-Path $PSScriptRoot scrapingclasses.ps1)
 
 clear
 
-$classes = gatherClasses
-$classes
+$FullTable = gatherClasses
+$FullTable = daystranslator($FullTable)
+
+#$FullTable | Select-Object "Class Code", Instructor, Location, Days, "Time Start", "Time End" |
+ #Where-Object{$_."Instructor" -ilike "Furkan Paligu" }
+
+#$FullTable | Select-Object { ($_.Location -ilike "JOYC 310") -and ($_.days -ilike "Monday*") } | `
+#            Sort-Object "Time Start" | `
+#            Format-Table "Time Start", "Time End", "Class Code"
+
+$ITSInstructors = $FullTable | Where-Object { ($_."Class Code" -ilike "SYS*" ) -or `
+                                               ($_."Class Code" -ilike "NET*" ) -or `
+                                               ($_."Class Code" -ilike "SEC*" ) -or `
+                                               ($_."Class Code" -ilike "FOR*" ) -or `
+                                               ($_."Class Code" -ilike "CSI*" ) -or `
+                                               ($_."Class Code" -ilike "DAT*" ) } `
+                             | Select-Object "Instructor" `
+                             #| Sort-Object "Instructor" -Unique
+
+#$ITSInstructors
+
+$FullTable | Where-Object { $_.Instructor -in $ITSInstructors.Instructor } `
+            | Group-Object "Instructor" | Select-Object Count,Name | Sort-Object Count -Descending
+
+
+
+
+                                                    
--- scrapingclasses.ps1
+++ scrapingclasses.ps1
@@ -11,7 +11,7 @@ function gatherClasses(){
     for($i=1; $i -lt $trs.length; $i++){
         $tds = $trs[$i].getElementsByTagName("td")
 
-        $Times = $tds[5].innerText.split(" ")
+        $Times = $tds[5].innerText.split("-")
 
         $FullTable += [pscustomobject]@{"Class Code" = $tds[0].innerText; # use innerText on these elements and fix indexes
                                         "Title"      = $tds[1].innerText;
@@ -26,9 +26,23 @@ function gatherClasses(){
 
 }
 
+
 function daysTranslator($FullTable){
 for ($i=0; $i -lt $FullTable.length; $i++){
     $Days = @()
 
-    if($FullTable[$i].Days -ilike "M"){ $days += "Monday" }
+    if($FullTable[$i].Days -ilike "M*"){ $Days += "Monday" }
+
+    if($FullTable[$i].Days -ilike "*T[TWF]*"){ $Days += "Tuesday" }
+    ElseIf($FullTable[$i].Days -ilike "T"){ $Days += "Tuesday"}
+
+    if($FullTable[$i].Days -ilike "*W*"){ $Days += "Wednesday"  }
+
+    if($FullTable[$i].Days -ilike "*TH*"){ $Days += "Thursday" }
+
+    if($FullTable[$i].Days -ilike "*F"){ $Days += "Friday" }
+
+    $FullTable[$i].Days = $Days
+}
+return $FullTable
 }
